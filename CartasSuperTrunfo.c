b2547e69e04d41ad509e5ae7765a6d563f63e036
Rfos21 <rfossk@gmail.com>
2025-01-11 18:33:48 +0000

    Foi adicionado a variável estado, atualizando o sistema para receber a informação do Estado e posteriomente mostrando o mesmo na ficha de dados de cada carta.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,28 +1,15 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-// Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-
-
 int main() {
     int populacao, pontoTuristico, pibCidade;
     float areaCidade;
     char nomeCidade[50], codCidade[50];
+    char estado;
+    
     
     printf("Cadastro de cartas do jogo Super Trunfo!!!\n");
+    printf("Digite o nome do estado, podendo ser apenas uma sigla de A a H:\n ");
+    scanf("%c", &estado);
     printf("Digite o nome da cidade:\n ");
     scanf("%s", &nomeCidade);
     printf("Digite o código da cidade:\n ");
@@ -35,6 +22,7 @@ int main() {
     scanf("%d", &pibCidade);
     printf("Digite a quantidade de pontos turísticos da cidade:\n ");
     scanf("%d", &pontoTuristico);
+    printf("Estado: %c\n", estado);
     printf("Cidade: %s\n", nomeCidade);
     printf("Código da cidade: %s\n", codCidade);
     printf("Populção: %d\n", populacao);
